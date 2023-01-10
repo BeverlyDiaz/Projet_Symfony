d7193f2e270dcfe2fdf7a69a53b1ef6bb709a10a
Beverly <beverly.diaz@hotmail.com>
2023-01-10 09:40:32 +0100

    Partie_3

---
+++ assets/Partie_3.js
@@ -0,0 +1,63 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/Partie_2.css';
+
+// start the Stimulus application
+import './bootstrap';
+//load in load page
+try {
+  var query = window.location.pathname
+  var result = query.slice(8);
+  getimage(result)
+} catch (error) {
+  console.log('err!')
+}
+
+document.querySelector('#btn').addEventListener('click', function () {
+  var result = ""; //debug result
+  getimage(result);
+})
+
+function getimage(result) {
+  document.querySelector("#err").innerHTML = ""
+  document.querySelectorAll("img[id='image']")[0].style.display = "inline"
+
+  try {
+    if (result.length == 0) {
+      const q = document.querySelector('#Search').value
+    } else {
+      var q = result;
+    }
+    fetch(`https://images-api.nasa.gov/search?q=${q}&media_type=image`)
+      .then(res => res.json())
+      .then(data => {
+
+
+        const Resp = data.collection.items.map(items => items.data[0].nasa_id);
+
+        const numbers = data.collection.items.length
+        var min = 0;
+        var max = numbers;
+        var i = Math.floor(Math.random() * (max - min)) + min;
+
+        try {
+          fetch(`https://images-api.nasa.gov/asset/${Resp[i]}`)
+            .then(res => res.json())
+            .then(data => {
+              document.querySelector("#image").src = data.collection.items[1].href;
+              return data.collection.items[1].href;
+
+            })
+        } catch (error) {
+        }
+
+      })
+  } catch (error) {
+  }
+}
